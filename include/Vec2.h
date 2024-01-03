50e45a602d71aa264c0e57d714a15da03c630216
ThatDevShparki <thatdevshparki@gmail.com>
2024-01-03 11:36:21 -0600

    vectors

---
+++ include/Vec2.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+class Vec2
+{
+	float x = 0.0f;
+	float y = 0.0f;
+
+public:
+	Vec2();
+	Vec2(float x, float y);
+
+	[[nodiscard]] Vec2 operator +(const Vec2& rhs) const;
+	[[nodiscard]] Vec2 operator -(const Vec2& rhs) const;
+	[[nodiscard]] Vec2 operator *(float rhs) const;
+	[[nodiscard]] Vec2 operator /(float rhs) const;
+
+	void operator +=(const Vec2& rhs);
+	void operator -=(const Vec2& rhs);
+	void operator *=(float rhs);
+	void operator /=(float rhs);
+
+	[[nodiscard]] float mag() const;
+	[[nodiscard]] Vec2 norm() const;
+	[[nodiscard]] float dist(const Vec2& rhs);
+};
---
+++ src/Vec2.cpp
@@ -0,0 +1,72 @@
+#include "Vec2.h"
+
+#include <cmath>
+
+Vec2::Vec2() = default;
+
+Vec2::Vec2(float x, float y)
+	: x(x), y(y)
+{
+}
+
+
+Vec2 Vec2::operator +(const Vec2& rhs) const
+{
+	return { x + rhs.x, y + rhs.y };
+}
+
+Vec2 Vec2::operator -(const Vec2& rhs) const
+{
+	return { x - rhs.x, y - rhs.y };
+}
+
+Vec2 Vec2::operator *(float rhs) const
+{
+	return { x * rhs, y * rhs };
+}
+
+Vec2 Vec2::operator /(float rhs) const
+{
+	return { x / rhs, y / rhs };
+}
+
+
+void Vec2::operator +=(const Vec2& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+}
+
+void Vec2::operator -=(const Vec2& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+}
+
+void Vec2::operator *=(float rhs)
+{
+	x *= rhs;
+	y *= rhs;
+}
+
+void Vec2::operator /=(float rhs)
+{
+	x /= rhs;
+	y /= rhs;
+}
+
+
+float Vec2::mag() const
+{
+	return sqrtf(powf(x, 2) + powf(y, 2));
+}
+
+Vec2 Vec2::norm() const
+{
+	return *this / mag();
+}
+
+float Vec2::dist(const Vec2& rhs)
+{
+	return (rhs - *this).mag();
+}
