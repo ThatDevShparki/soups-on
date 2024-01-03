3437dd2b9509df5cdf90af4faae1a155bf36027d
ThatDevShparki <thatdevshparki@gmail.com>
2024-01-03 12:29:30 -0600

    entity manager

--- include/EntityManager.h
+++ include/EntityManager.h
@@ -1,8 +1,32 @@
 #pragma once
 
+#include "Entity.h"
 #include "Vec2.h"
+
+#include <map>
+#include <string>
+#include <vector>
+
+typedef std::vector<std::shared_ptr<Entity>> EntityVec;
+typedef std::map<std::string, EntityVec>     EntityMap;
 
 class EntityManager
 {
-	
+	EntityVec m_entities;
+	EntityVec m_entitiesToAdd;
+	EntityMap m_entityMap;
+	size_t    m_totalEntities = 0;
+
+	static void removeDeadEntities(EntityVec& vec);
+
+public:
+
+	EntityManager();
+
+	void update();
+
+	std::shared_ptr<Entity> addEntity(const std::string& tag);
+	[[nodiscard]] const EntityVec& getEntities() const;
+	[[nodiscard]] const EntityVec& getEntities(const std::string& tag) const;
+	[[nodiscard]] const EntityMap& getEntityMap() const;
 };
--- src/EntityManager.cpp
+++ src/EntityManager.cpp
@@ -1,1 +1,55 @@
 #include "EntityManager.h"
+
+EntityManager::EntityManager() = default;
+
+void EntityManager::update()
+{
+	// Add all entities queued for addition
+	for (auto& entity: m_entitiesToAdd)
+	{
+		m_entities.push_back(entity);
+		m_entityMap[entity->tag()].push_back(entity);
+	}
+
+	// Remove all entities that are no longer alive
+	removeDeadEntities(m_entities);
+	for (auto& [_, entities]: m_entityMap)
+	{
+		removeDeadEntities(entities);
+	}
+}
+
+void EntityManager::removeDeadEntities(EntityVec& vec)
+{
+	vec.erase(
+		std::remove_if(
+			vec.begin(), vec.end(), [](const auto& entity)
+			{
+				return !entity->isActive();
+			}
+		), vec.end());
+}
+
+
+std::shared_ptr<Entity> EntityManager::addEntity(const std::string& tag)
+{
+	auto entity = std::shared_ptr<Entity>(new Entity(m_totalEntities++, tag));
+	m_entitiesToAdd.push_back(entity);
+
+	return entity;
+}
+
+const EntityVec& EntityManager::getEntities() const
+{
+	return m_entities;
+}
+
+const EntityVec& EntityManager::getEntities(const std::string& tag) const
+{
+	return m_entityMap.at(tag);
+}
+
+const EntityMap& EntityManager::getEntityMap() const
+{
+	return m_entityMap;
+}
