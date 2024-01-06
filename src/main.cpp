7c60e0ddcf7c67a929e47dceaf7b6a9fd45743c9
ThatDevShparki <thatdevshparki@gmail.com>
2024-01-06 14:05:01 -0600

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
 #include "Game.h"
-#include "Map.h"
 
 int main()
 {
-//	Game game("assets/manifest.txt");
-//	game.run();
-
-
-	Map map({ "assets/maps/map_001._map.csv", "assets/maps/map_001._items.csv" });
-	map.print();
+	Game game("assets/manifest.txt");
+	game.run();
 }
